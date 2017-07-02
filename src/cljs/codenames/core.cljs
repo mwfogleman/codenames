18d4c431ed1c3506f63051a3258e1cc8579a1bb4
Michael Fogleman <michaelwfogleman@gmail.com>
2017-07-02 09:20:54 -0400

    Remove lots of code.

--- src/cljs/codenames/core.cljs
+++ src/cljs/codenames/core.cljs
@@ -452,7 +452,7 @@
 
 (defonce game (reagent/atom (prepare-game)))
 
-(defn new-game! [game]
+(defn new-game! []
   (reset! game (prepare-game)))
 
 ;; Game Play
@@ -462,206 +462,22 @@
   [collection element]
   (some #(= element %) collection))
 
-(defn word-filterer [w {:keys [word]}]
-  (= word w))
-
-(defn valid-word? [game word]
-  (let [words (S/select [S/ATOM :words S/ALL :word] game)]
-    (in? words word)))
-
-(defn revealed? [game word]
-  (S/select-any [S/ATOM :words (S/filterer #(word-filterer word %)) S/ALL :revealed?] game))
-
-(def hidden? (complement revealed?))
-
-(defn get-freqs [game]
-  (let [words (S/select [S/ATOM :words S/ALL] game)
-        get-attributes (juxt :identity :revealed?)]
-    (->> words
-         (map get-attributes)
-         (frequencies))))
-
-(defn reveal! [game word]
-  (S/setval [S/ATOM :words (S/filterer #(word-filterer word %)) S/ALL :revealed?]
-            true game))
-
-(defn next-round! [game]
-  (S/transform [S/ATOM :round] inc game))
-
-(defn opposite-team [team]
-  (if (= team :red)
-    :blue
-    :red))
-
-(defn switch-teams! [game]
-  (S/transform [S/ATOM :current-team] opposite-team game))
-
-(defn next-turn! [game]
-  (next-round! game)
-  (switch-teams! game))
-
-(defn win!
-  "Makes the current team win the game."
-  [game]
-  (let [winner (S/select-any [S/ATOM :current-team] game)]
-    (S/setval [S/ATOM :winning-team] winner game)))
-
-(defn lose!
-  "Makes the current team lose the game."
-  [game]
-  (let [loser  (S/select-any [S/ATOM :current-team] game)
-        winner (opposite-team loser)]
-    (S/setval [S/ATOM :winning-team] winner game)))
-
-(defn winner?
-  "If a GAME has a winner, return true. If not, return false."
-  [game]
-  (->> game
-       (S/select-any [S/ATOM :winning-team])
-       (some?)))
-
-(defn cell-filterer
-  [target {:keys [position]}]
-  (= target position))
-
-(defn get-cell
-  [game x y]
-  (S/select-any [:words (S/filterer #(cell-filterer [x y] %)) S/ALL] @game))
-
-(defn get-current-team
-  [game]
-  (:current-team @game))
-
-(defn get-revealed-status
-  [game x y]
-  (:revealed? (get-cell game x y)))
-
-(defn get-view
-  [game]
-  (:view @game))
-
-(defn get-winner
-  [game]
-  (:winning-team @game))
-
-(defn- get-id-of-word [game word]
-  (S/select-any [S/ATOM :words (S/filterer #(word-filterer word %)) S/ALL :identity] game))
-
-(defn get-remaining
-  [game]
-  (S/select-any [S/ATOM (S/submap [:blue-remaining :red-remaining])] game))
-
-(defn update-remaining!
-  [game]
-  (let [frqs           (get-freqs game)
-        blue-remaining (get frqs [:blue false])
-        red-remaining  (get frqs [:red false])]
-    (S/setval [S/ATOM :blue-remaining] blue-remaining game)
-    (S/setval [S/ATOM :red-remaining]  red-remaining game)))
-
-(defn move! [game word]
-  {:pre [(valid-word? game word)
-         (hidden? game word)
-         (= false (winner? game))]}
-  (reveal! game word)
-  (update-remaining! game)
-  (let [current-team                           (get-current-team game)
-        id                                     (get-id-of-word game word)
-        match-result                           (= id current-team) ;; Register whether they picked someone on their team, or on the other team.
-        {:keys [blue-remaining red-remaining]} (get-remaining game)]
-    (cond (= id :assassin) (lose! game)
-          (and (> blue-remaining 0) (> red-remaining 0)) ;; Check if there are remaining hidden cards for either team.
-          ;; If they picked someone on their team, they can keep moving
-          ;; If they picked someone from the other team, switch to make it the other team's turn.
-          (if (true? match-result)
-            game
-            (next-turn! game))
-          :else
-          (if (true? match-result)
-            ;; If the card picked was theirs, win!
-            (win! game)
-            ;; Otherwise, lose!
-            (lose! game)))))
-
 ;; -------------------------
 ;; Views
 
-(defn colorize [word identity]
-  (case identity
-    :blue     [:div {:style {:color "blue"}}
-               word]
-    :red      [:div {:style {:color "red"}}
-               word]
-    :assassin [:div {:style {:color "grey"}}
-               word]
-    :neutral  [:div {:style {:color "yellow"}}
-               word]))
-
-(defn cell [game x y]
-  (let [{:keys [word identity revealed?]} (get-cell game x y)
-        winner                            (get-winner game)]
-    (fn []
-      (if winner
-        [:span
-         [colorize word identity]]
-        (if (true? revealed?)
-          [:span {:style {:width 30
-                          :height 30}}
-           [colorize word identity]]
-          [:button {:on-click #(move! game word)
-                    :style {:width 100
-                            :height 100}}
-           [:div {:style {:color "black"}}
-            word]])))))
-
-(defn grid [game]
-  [:table
-   (for [y (range 5)]
-     [:tr
-      (for [x (range 5)]
-        [:td {:style {:width      100
-                      :height     100
-                      :text-align :center}}
-         [cell game x y]])])])
-
-(defn main-panel [game]
-  (let [turn   (get-current-team game)
-        winner (get-winner game)]
-    (fn []
-      [:div
-       (if winner
-         [:div
-          (clojure.string/capitalize (name winner)) " is the winner."]
-         [:div
-          "It's " (name turn) "'s turn."])
-       [:center
-        [:p
-         [grid game]]]
-       [:p
-        game]])))
-
 (defn home-page []
-  [:div [:h2 "Welcome to Codenames"]
-   [:div [:a {:href "/game"} "Play a game!"]]])
-
-(defn game-page []
-  (let [game (reagent/atom (prepare-game))]
-    [:div [:h2 "Codenames"]
-     [main-panel game]]))
+  [:div [:h2 "Codenames"]])
 
 ;; -------------------------
 ;; Routes
 
-(def page (reagent/atom #'game-page))
+(def page (reagent/atom #'home-page))
 
 (defn current-page []
   [:div [@page]])
 
 (secretary/defroute "/" []
-  (reset! page #'game-page))
-
-(secretary/defroute "/game" []
-  (reset! page #'game-page))
+  (reset! page #'home-page))
 
 ;; -------------------------
 ;; Initialize app
