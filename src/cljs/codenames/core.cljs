61e5f66eecded7ffc9c66a072488a8ce27c4186d
Michael Fogleman <michaelwfogleman@gmail.com>
2017-08-22 08:44:56 -0400

    Add architecture for debugging the sample game.

--- src/cljs/codenames/core.cljs
+++ src/cljs/codenames/core.cljs
@@ -3,40 +3,43 @@
             [secretary.core :as secretary :include-macros true]
             [accountant.core :as accountant]))
 
-(def sample-game
-  (atom {:starting-team :red,
-         :blue-remaining 8,
-         :current-team :red,
-         :words
-         '({:word "IVORY", :identity :red, :revealed? false, :position [3 1]}
-           {:word "QUEEN", :identity :red, :revealed? false, :position [3 0]}
-           {:word "HEART", :identity :red, :revealed? false, :position [0 2]}
-           {:word "KETCHUP", :identity :red, :revealed? false, :position [2 0]}
-           {:word "SPOT", :identity :red, :revealed? false, :position [0 4]}
-           {:word "COMIC", :identity :red, :revealed? false, :position [4 3]}
-           {:word "FIGURE", :identity :red, :revealed? false, :position [4 0]}
-           {:word "MARCH", :identity :red, :revealed? false, :position [2 3]}
-           {:word "THUMB", :identity :red, :revealed? false, :position [3 3]}
-           {:word "PIANO", :identity :blue, :revealed? false, :position [0 0]}
-           {:word "STATE", :identity :blue, :revealed? false, :position [1 0]}
-           {:word "SPELL", :identity :blue, :revealed? false, :position [2 2]}
-           {:word "BUFFALO", :identity :blue, :revealed? false, :position [1 1]}
-           {:word "TOKYO", :identity :blue, :revealed? false, :position [4 2]}
-           {:word "DRAGON", :identity :blue, :revealed? false, :position [3 4]}
-           {:word "MOSCOW", :identity :blue, :revealed? false, :position [0 1]}
-           {:word "PENGUIN", :identity :blue, :revealed? false, :position [0 3]}
-           {:word "SATELLITE", :identity :neutral, :revealed? false, :position [1 2]}
-           {:word "DEATH", :identity :neutral, :revealed? false, :position [1 3]}
-           {:word "CHICK", :identity :neutral, :revealed? false, :position [4 1]}
-           {:word "RABBIT", :identity :neutral, :revealed? false, :position [4 4]}
-           {:word "COPPER", :identity :neutral, :revealed? false, :position [2 1]}
-           {:word "POOL", :identity :neutral, :revealed? false, :position [2 4]}
-           {:word "ORGAN", :identity :neutral, :revealed? false, :position [1 4]}
-           {:word "LEPRECHAUN", :identity :assassin, :revealed? false, :position [3 2]}),
-         :red-remaining 9,
-         :round 0,
-         :id "G__54236",
-         :winning-team nil}))
+(def initial-sample-value {:starting-team :red,
+                           :blue-remaining 8,
+                           :current-team :red,
+                           :words
+                           '({:word "IVORY", :identity :red, :revealed? false, :position [3 1]}
+                             {:word "QUEEN", :identity :red, :revealed? false, :position [3 0]}
+                             {:word "HEART", :identity :red, :revealed? false, :position [0 2]}
+                             {:word "KETCHUP", :identity :red, :revealed? false, :position [2 0]}
+                             {:word "SPOT", :identity :red, :revealed? false, :position [0 4]}
+                             {:word "COMIC", :identity :red, :revealed? false, :position [4 3]}
+                             {:word "FIGURE", :identity :red, :revealed? false, :position [4 0]}
+                             {:word "MARCH", :identity :red, :revealed? false, :position [2 3]}
+                             {:word "THUMB", :identity :red, :revealed? false, :position [3 3]}
+                             {:word "PIANO", :identity :blue, :revealed? false, :position [0 0]}
+                             {:word "STATE", :identity :blue, :revealed? false, :position [1 0]}
+                             {:word "SPELL", :identity :blue, :revealed? false, :position [2 2]}
+                             {:word "BUFFALO", :identity :blue, :revealed? false, :position [1 1]}
+                             {:word "TOKYO", :identity :blue, :revealed? false, :position [4 2]}
+                             {:word "DRAGON", :identity :blue, :revealed? false, :position [3 4]}
+                             {:word "MOSCOW", :identity :blue, :revealed? false, :position [0 1]}
+                             {:word "PENGUIN", :identity :blue, :revealed? false, :position [0 3]}
+                             {:word "SATELLITE", :identity :neutral, :revealed? false, :position [1 2]}
+                             {:word "DEATH", :identity :neutral, :revealed? false, :position [1 3]}
+                             {:word "CHICK", :identity :neutral, :revealed? false, :position [4 1]}
+                             {:word "RABBIT", :identity :neutral, :revealed? false, :position [4 4]}
+                             {:word "COPPER", :identity :neutral, :revealed? false, :position [2 1]}
+                             {:word "POOL", :identity :neutral, :revealed? false, :position [2 4]}
+                             {:word "ORGAN", :identity :neutral, :revealed? false, :position [1 4]}
+                             {:word "LEPRECHAUN", :identity :assassin, :revealed? false, :position [3 2]}),
+                           :red-remaining 9,
+                           :round 0,
+                           :id "G__54236",
+                           :winning-team nil})
+
+(def sample-game (-> initial-sample-value atom))
+
+(defn reset-sample-game! [] (reset! sample-game initial-sample-value))
 
 ;; -------------------------
 ;; Views
