b69562efd7ec28bfe8480c7530882f150f20275d
Vinai Kopp <vinai@netzarbeiter.com>
2017-11-01 17:16:12 +0100

    Spec round to be a positive int.

--- src/clj/codenames/handler.clj
+++ src/clj/codenames/handler.clj
@@ -24,11 +24,18 @@
 (def Words
   [(s/one Word 1) (s/one Word 2) (s/one Word 3) (s/one Word 4) (s/one Word 5) (s/one Word 6) (s/one Word 7) (s/one Word 8) (s/one Word 9) (s/one Word 10) (s/one Word 11) (s/one Word 12) (s/one Word 13) (s/one Word 14) (s/one Word 15) (s/one Word 16) (s/one Word 17) (s/one Word 18) (s/one Word 19) (s/one Word 20) (s/one Word 21) (s/one Word 22) (s/one Word 23) (s/one Word 24) (s/one Word 25)])
 
+(defn positive-int? [n]
+  (and (integer? n) (>= n 0)))
+
+(def Round
+  (s/pred positive-int?))
+
 (def Game
   {:starting-team Team
    :current-team Team
    :remaining Remaining
    :winning-team (s/maybe Team)
+   :round Round
    :words Words})
 
 (def mount-target
