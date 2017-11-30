d0a76f9df1e2cb65fe421a2bb1335e550f6970f3
Michael Fogleman <michaelwfogleman@gmail.com>
2017-11-29 19:44:06 -0500

    Add Sente.

--- project.clj
+++ project.clj
@@ -21,7 +21,8 @@
                  [secretary "1.2.3"]
                  [venantius/accountant "0.2.0"
                   :exclusions [org.clojure/tools.reader]]
-                 [yogthos/config "0.9"]]
+                 [yogthos/config "0.9"]
+                 [com.taoensso/sente "1.11.0"]]
 
   :plugins [[lein-environ "1.0.2"]
             [lein-cljsbuild "1.1.5"]
