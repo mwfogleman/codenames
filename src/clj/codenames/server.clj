51bc2cce7cb5fca7be7976793e55431fa7470f95
Michael Fogleman <michaelwfogleman@gmail.com>
2017-12-06 20:02:27 -0500

    Remove convenience functions in preparation for Jetty -> Immutant.

--- src/clj/codenames/server.clj
+++ src/clj/codenames/server.clj
@@ -4,22 +4,6 @@
             [ring.adapter.jetty :refer [run-jetty]])
   (:gen-class))
 
- (defn -main [& args]
-   (let [port (Integer/parseInt (or (env :port) "3000"))]
-     (run-jetty app {:port port :join? false})))
-
-
-(defonce dev-server (atom nil))
-
-(defn go-dev-server
-  [] (when (not @dev-server)
-       (reset! dev-server (-main))))
-
-(defn stop-dev-server []
-  (when @dev-server
-    (.stop @dev-server)
-    (reset! dev-server nil)))
-
-(defn restart-dev-server []
-  (stop-dev-server)
-  (go-dev-server))
+(defn -main [& args]
+  (let [port (Integer/parseInt (or (env :port) "3000"))]
+    (run-jetty app {:port port :join? false})))
