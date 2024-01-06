6e574e1ec3fad9116691fd0dfa14b968ab18ff2b
Tasshin Fogleman <tasshinfogleman@gmail.com>
2024-01-06 14:19:54 -0800

    Update and sort dependencies.

--- project.clj
+++ project.clj
@@ -4,25 +4,25 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [ring-server "0.5.0"]
-                 [reagent "1.1.1"]
-                 [reagent-utils "0.3.4"]
-                 [cljsjs/react "17.0.2-0"]
-                 [cljsjs/react-dom "17.0.2-0"]
-                 [ring "1.9.5"]
-                 [ring/ring-defaults "0.3.3"]
+  :dependencies [[cljsjs/react "18.2.0-1"]
+                 [cljsjs/react-dom "18.2.0-1"]
                  [hiccup "1.0.5"]
-                 [yogthos/config "1.2.0"]
-                 [org.clojure/clojurescript "1.11.54"
+                 [metosin/reitit "0.6.0"]
+                 [org.clojure/clojure "1.11.1"]
+                 [org.clojure/clojurescript "1.11.60"
                   :scope "provided"]
-                 [metosin/reitit "0.5.18"]
                  [pez/clerk "1.0.0"]
+                 [reagent "1.2.0"]
+                 [reagent-utils "0.3.8"]
+                 [ring "1.11.0"]
+                 [ring-server "0.5.0"]
+                 [ring/ring-defaults "0.4.0"]
                  [venantius/accountant "0.2.5"
-                  :exclusions [org.clojure/tools.reader]]]
+                  :exclusions [org.clojure/tools.reader]]
+                 [yogthos/config "1.2.0"]]
 
   :jvm-opts ["-Xmx1G"]
-  
+
   :plugins [[lein-environ "1.1.0"]
             [lein-cljsbuild "1.1.7"]
             [lein-asset-minifier "0.4.6"
@@ -86,19 +86,16 @@
 
   :profiles {:dev {:repl-options {:init-ns codenames.repl}
                    :dependencies [[cider/piggieback "0.5.3"]
-                                  [binaryage/devtools "1.0.6"]
+                                  [binaryage/devtools "1.0.7"]
                                   [ring/ring-mock "0.4.0"]
-                                  [ring/ring-devel "1.9.5"]
+                                  [ring/ring-devel "1.11.0"]
                                   [prone "2021-04-23"]
                                   [figwheel-sidecar "0.5.20"]
-                                  [nrepl "0.9.0"]
-                                  [pjstadig/humane-test-output "0.11.0"]
-                                  
- ]
+                                  [nrepl "1.1.0"]
+                                  [pjstadig/humane-test-output "0.11.0"]]
 
                    :source-paths ["env/dev/clj"]
-                   :plugins [[lein-figwheel "0.5.20"]
-]
+                   :plugins [[lein-figwheel "0.5.20"]]
 
                    :injections [(require 'pjstadig.humane-test-output)
                                 (pjstadig.humane-test-output/activate!)]
