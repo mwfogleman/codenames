5d7eb513a90614d6060ecc572fceaf19e3591c78
Tasshin Fogleman <tasshinfogleman@gmail.com>
2024-02-01 12:15:40 -0800

    Switch run-jetty to httpkit/run-server.

--- src/clj/codenames/server.clj
+++ src/clj/codenames/server.clj
@@ -1,10 +1,10 @@
 (ns codenames.server
-    (:require
-     [codenames.handler :refer [app]]
-     [config.core :refer [env]]
-     [ring.adapter.jetty :refer [run-jetty]])
-    (:gen-class))
+  (:require
+   [codenames.handler :refer [app]]
+   [config.core :refer [env]]
+   [org.httpkit.server :as httpkit :refer [run-server]])
+  (:gen-class))
 
 (defn -main [& args]
   (let [port (or (env :port) 3000)]
-    (run-jetty #'app {:port port :join? false})))
+    (run-server #'app {:port port :join? false})))
